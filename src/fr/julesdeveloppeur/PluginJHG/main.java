83a78f0b37f4eeac500a06ddbcfbdcd25eaa2ef1
Julesdev <julesmairesse59@gmail.com>
2022-05-04 10:43:09 +0200

    Ajout de la commande /test

--- src/fr/julesdeveloppeur/PluginJHG/main.java
+++ src/fr/julesdeveloppeur/PluginJHG/main.java
@@ -2,11 +2,14 @@ package fr.julesdeveloppeur.PluginJHG;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.julesdeveloppeur.PluginJHG.commands.CommmandTest;
+
 public class main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
 		System.out.println("Le plugin est prêt !");
+		getCommand("test").setExecutor(new CommmandTest());
 	}
 	
 	@Override
